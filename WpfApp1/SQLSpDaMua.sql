9d6c015cf97c84ed4b603879740dbbe267e8211b
Hoang <134166368+koi132@users.noreply.github.com>
2024-03-17 15:22:50 +0000

    add SQL sản phẩm và sản phẩm đã mua

---
+++ WpfApp1/SQLSpDaMua.sql
@@ -0,0 +1,13 @@
+﻿SELECT *
+INTO SanPhamDaMua
+FROM SanPham
+WHERE 1=0
+
+INSERT INTO SanPhamDaMua (MaSP, TenSP, TenShop, GiaGoc, GiaHTai, NgayMua, ConMoi, MoTa, HinhAnh)
+VALUES ('AAAA', 'Xe May', 'Sg', 17600000.5, 10000, '2017-07-26', 98, 'khong co mo ta', 'https://drive.google.com/file/d/1yFLF4LX2Ql0dLSKj2_yjXmVjIrmkdFpG/view?usp=sharing')
+INSERT INTO SanPhamDaMua (MaSP, TenSP, TenShop, GiaGoc, GiaHTai, NgayMua, ConMoi, MoTa, HinhAnh)
+VALUES ('AAAA', 'Oto', 'Hn', 19900000, 15000, '2020-11-12', 97, 'khong co mo ta', 'https://drive.google.com/file/d/1yFLF4LX2Ql0dLSKj2_yjXmVjIrmkdFpG/view?usp=sharing')
+INSERT INTO SanPhamDaMua (MaSP, TenSP, TenShop, GiaGoc, GiaHTai, NgayMua, ConMoi, MoTa, HinhAnh)
+VALUES ('AAAA', 'May bay', 'Dn', 11400000.5, 9000, '2003-12-31', 99, 'khong co mo ta', 'https://drive.google.com/file/d/1yFLF4LX2Ql0dLSKj2_yjXmVjIrmkdFpG/view?usp=sharing')
+
+SELECT * FROM SanPhamDaMua
