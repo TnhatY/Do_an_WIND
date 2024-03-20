ee760596f5c7574cdef6d58ac86b9484fe2a4ba8
Hoang <134166368+koi132@users.noreply.github.com>
2024-03-20 22:54:46 +0700

    hoang push code

--- WpfApp1/UC_gioHang.xaml.cs
+++ WpfApp1/UC_gioHang.xaml.cs
@@ -1,6 +1,7 @@
-﻿using Do_an;
+using Do_an;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,42 +14,56 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using static MaterialDesignThemes.Wpf.Theme;
+using System.Data.SqlClient;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for UC_gioHang.xaml
-    /// </summary>
-    public partial class UC_gioHang : UserControl
-    {
-        public UC_gioHang()
-        {
-            InitializeComponent();
-            listview.Items.Add(new Class1 { Id = 1, name = "anh", shop = "dsgf", gia = 1243 });
-        }
+	public partial class UC_gioHang : UserControl
+	{
+		public ObservableCollection<UC_SpGioHang> SanPhamList { get; set; }
 
-        private void btnMua_Click(object sender, RoutedEventArgs e)
-        {
-            ThanhToan_Window thanhToan_Window = new ThanhToan_Window();
-            thanhToan_Window.Show();
-        }
+		public UC_gioHang()
+		{
+			InitializeComponent();
+			SanPhamList = new ObservableCollection<UC_SpGioHang>();
+			
+			this.DataContext = this;
+			this.Loaded += UC_gioHang_Loaded;
+		}
 
-        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            object selectedItem = listview.SelectedItem;
-            if (selectedItem != null)
-            {
-                ThongTin t = new ThongTin();
-                if (selectedItem is Class1 selectedC)
-                {
-                    t.MaSP.Text = selectedC.Id.ToString();
-                    t.Ten.Text = selectedC.name.ToString();
-                    t.shop.Text = selectedC.shop.ToString();
-                    t.gia.Text = selectedC.gia.ToString();
-                }
-                t.ShowDialog();
-            }
-        }
-    }
+		private void UC_gioHang_Loaded(object sender, RoutedEventArgs e)
+		{
+			LoadDataFromDatabase();
+		}
+
+		private void LoadDataFromDatabase()
+		{
+			SqlConnection conn = new SqlConnection(Do_an.Properties.Settings.Default.connStr);
+			{
+				conn.Open();
+
+				using (SqlCommand command = new SqlCommand("SELECT * FROM SanPham", conn))
+				using (SqlDataReader reader = command.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						UC_SpGioHang sp = new UC_SpGioHang();
+
+						sp.lblTenSP.Content = reader["TenSP"].ToString();
+						sp.lblTenShop.Content = reader["TenShop"].ToString();
+						sp.lblGiaGoc.Content = reader["GiaGoc"].ToString();
+						sp.lblGiaHTai.Content = reader["GiaHTai"].ToString();
+						sp.lblTienThanhToan.Content = reader["GiaHTai"].ToString();
+
+						SanPhamList.Add(sp);
+					}
+				}
+			}
+		}
+		public void ReloadData()
+		{
+			SanPhamList.Clear();
+			LoadDataFromDatabase();
+		}
+	}
 }
