60fe885bd0acd66b41a263644ffae4493c7a0cc4
huanchaunhat <huanchaunhat@gmail.com>
2024-03-20 20:49:22 +0700

    HuanDepTraiVCL

--- WpfApp1/ThemSP_Window.xaml.cs
+++ WpfApp1/ThemSP_Window.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,14 +20,43 @@ namespace Do_an
     /// </summary>
     public partial class ThemSP_Window : Window
     {
+
         public ThemSP_Window()
         {
             InitializeComponent();
         }
-
+        List<string> DanhMuc = new List<string> { "Điện thọai", "Đồ gia dụng", "Xe cộ", "Đồ điện tử", "Đồ điện gia dụng", "Thời trang" };
         private void btnThoat_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void Image_Button(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files|*.jpg;*.png";
+            openFileDialog.FilterIndex = 2;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                imgDynamic.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+        }
+
+        void cbDanhMuc_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            cbDanhMuc.ItemsSource = DanhMuc;
+            cbDanhMuc.SelectionChanged += cbDanhMuc_SelectionChanged;
+        }
+
+        private void txtMoTa_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btnThem_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
